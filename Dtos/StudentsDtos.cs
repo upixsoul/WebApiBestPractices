ca75d8b92003e88f38a0093be32f51714099af68
David Alejandro Garcia Garcia <upixsoul@gmail.com>
2026-03-08 14:45:46 -0600

    project change name

--- Dtos/StudentsDtos.cs
+++ Dtos/StudentsDtos.cs
@@ -1,8 +1,11 @@
 ﻿namespace StudentsApi.Dtos
 {
-    public record CreateStudentDto(string FirstName, string LastName, DateTime DateOfBirth);
-    public record StudentResponseDto(int Id, string FirstName, string LastName, DateTime DateOfBirth);
+    public record CreateStudentDto(string FirstName, string LastName, 
+        DateTime DateOfBirth);
+    public record StudentResponseDto(int Id, string FirstName, 
+        string LastName, DateTime DateOfBirth);
 
     public record CreateQualificationDto(string Subject, decimal Grade);
-    public record QualificationResponseDto(int Id, string Subject, decimal Grade, DateTime Date);
+    public record QualificationResponseDto(int Id, string Subject, 
+        decimal Grade, DateTime Date);
 }
